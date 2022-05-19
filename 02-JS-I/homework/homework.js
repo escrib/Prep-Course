92a1455001e6056bb618b8d94ae1f870739b9f37
escrib <sebascraton@gmail.com>
2022-05-18 23:30:36 -0300

    1-105

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -94,12 +94,14 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
-}
+    if  (num>50) {return true;}
+    else {return false;}
+  }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+    return (x%y);
   
 }
 
@@ -107,6 +109,8 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+    if (num%2===0) {return true;}
+    else {return false;}
   
 }
 
@@ -114,6 +118,8 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+    if (num%2 === 1) {return true;}
+    else {return false;}
   
 }
 
@@ -121,12 +127,14 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+    return(num*num);
   
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+    return(num*num*num);
   
 }
 
